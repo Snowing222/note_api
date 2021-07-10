e51cd0b133ae987e7a88c5357ea2163ece13ceae
snowing222 <snowing222@gmail.com>
2021-07-10 12:20:10 -0400

    finish api building

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Note.create([
+    {body: "post1"},
+    {body: "post2"},
+    {body: "post3"},
+    {body: "post4"},
+    {body: "post5"}
+
+])
